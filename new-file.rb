2c6661bb8a07ec281f6bf003a860db46f4fecb3c
Cherie Lampley <cherie@cherielampley.com>
2015-03-30 19:38:02 -0400

    My first commit on the new branch

---
+++ new-file.rb
@@ -0,0 +1,6 @@
+book_one = {title: "Vicious", author: "V.E. Schwab"}
+book_two = {title: "The Bone Season", author: "Samantha Shannon"}
+book_three = {title: "Clockwork Prince", author: "Cassandra Clare"}
+
+
+puts book_one[:title]
